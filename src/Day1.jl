8c5d7e4ab7a735c3e7e52433427af5ab62f56bfb
Neal Grantham <neal@nsgrantham.com>
2023-10-19 16:59:49 -0700

    Add Day 1

--- src/Day1.jl
+++ src/Day1.jl
@@ -1,14 +1,22 @@
 module Day1
 
 using AdventOfCode2020
+using Combinatorics
+
+
+function get_triplet_summing_to(x, n)
+    for triplet in combinations(x, 3)
+        sum(triplet) == n && return triplet
+    end
+end
+
 
 function solve(input=pkgdir(AdventOfCode2020, "data", "Day1.txt"))
-    p1 = 0
-    p2 = 1
-
-    for line in eachline(input)
-        # do something
-    end
+    x = parse.(Int, readlines(input))
+    y = 2020 .- x
+    
+    p1 = prod(intersect(Set(x), Set(y)))
+    p2 = prod(get_triplet_summing_to(x, 2020))
 
     p1, p2
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,14 @@ using Test
 
 
 @testset "Day 1" begin
-    example = ""
+    example = """
+    1721
+    979
+    366
+    299
+    675
+    1456
+    """
 
-    @test AdventOfCode2020.Day1.solve(IOBuffer(example)) == (0, 1)
+    @test AdventOfCode2020.Day1.solve(IOBuffer(example)) == (514579, 241861950)
 end
